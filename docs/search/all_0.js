20b14b264fbc34c6ee5108a9f3d2dcf22517d3b7
ntppro <mr.trongphuongpro@gmail.com>
2020-04-11 14:23:41 +0300

    Update docs

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['atmega_5fuart_5fopen_0',['atmega_uart_open',['../uart_8h.html#a55bdae1a55fb33e7120a89e55613ea59',1,'atmega_uart_open(uint32_t baudrate):&#160;uart_atmega.c'],['../uart__atmega_8c.html#a55bdae1a55fb33e7120a89e55613ea59',1,'atmega_uart_open(uint32_t baudrate):&#160;uart_atmega.c']]]
+  ['atmega_5fuart_5finit_0',['atmega_uart_init',['../uart_8h.html#a66c0184ab004c405715f86b5619363fb',1,'atmega_uart_init(uint32_t baudrate):&#160;uart_atmega.c'],['../uart__atmega_8c.html#a66c0184ab004c405715f86b5619363fb',1,'atmega_uart_init(uint32_t baudrate):&#160;uart_atmega.c']]]
 ];
--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['tiva_5fuart_5fopen_2',['tiva_uart_open',['../uart_8h.html#a9e6eccb1e01b0a7be965357d14cc3e77',1,'tiva_uart_open(uint32_t base):&#160;uart_tiva.c'],['../uart__tiva_8c.html#a9e6eccb1e01b0a7be965357d14cc3e77',1,'tiva_uart_open(uint32_t base):&#160;uart_tiva.c']]]
+  ['tiva_5fuart_5finit_2',['tiva_uart_init',['../uart_8h.html#a28e8262ac32cfdbd414673aa7080e83c',1,'tiva_uart_init(uint32_t base, uint32_t baudrate):&#160;uart_tiva.c'],['../uart__tiva_8c.html#a28e8262ac32cfdbd414673aa7080e83c',1,'tiva_uart_init(uint32_t base, uint32_t baudrate):&#160;uart_tiva.c']]]
 ];
